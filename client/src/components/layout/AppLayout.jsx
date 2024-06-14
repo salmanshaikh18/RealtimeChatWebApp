7e7878d0bb6f796a16e882f822aaf29755cc86c7
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-14 23:40:36 +0530

    update applyout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Title from "../shared/Title";
+import { Grid } from "@mui/material";
 
-const AppLayout = () => WrappedComponent => {
+const AppLayout = () => (WrappedComponent) => {
   return (props) => (
     <div>
-        <Header />
-        <WrappedComponent {...props} />
-        <div>Footer</div>
-        <Footer />
+      <Title />
+      <Header />
+      <Grid container height={"calc(100vh - 4rem)"}>
+        <Grid
+          item
+          sm={4}
+          md={3}
+          sx={{
+            display: { xs: "none", sm: "block" },
+          }}
+          height={"100%"}
+          bgcolor="primary.main"
+        >
+          First
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          lg={6}
+          height={"100%"}
+          bgcolor="primary.main"
+        >
+          <WrappedComponent {...props} />
+        </Grid>
+        <Grid
+          item
+          md={4}
+          lg={3}
+          sx={{
+            display: { xs: "none", sm: "block" },
+            padding: "2rem",
+            bgcolor: "rgba(0,0,0,0.85)",
+          }}
+          height={"100%"}
+          bgcolor="primary.main"
+        >
+          Third
+        </Grid>
+      </Grid>
+      <WrappedComponent {...props} />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
