fafff3f6ba0eef8f1cf13e3b21c13783607f6316
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-06-14 20:46:27 +0530

    login page + validator

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,210 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+
+import { useInputValidation } from "6pp";
+import { usernameValidator } from "../utils/validators/validators";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-export default Login
+  const name = useInputValidation("");
+  const bio = useInputValidation("");
+  const username = useInputValidation("", usernameValidator);
+  const password = useInputValidation("");
+
+  return (
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLoggedIn ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                fullWidth
+                color="secondary"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Login
+              </Button>
+
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="text"
+                fullWidth
+                onClick={() => setIsLoggedIn(!isLoggedIn)}
+              >
+                Sign Up Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Sign Up</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <Stack position={"relative"} width={"10rem"} margin="auto">
+                <Avatar
+                  sx={{
+                    width: "10rem",
+                    height: "10rem",
+                    objectFit: "contain",
+                  }}
+                />
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    bottom: "0",
+                    right: "0",
+                    color: "white",
+                    bgcolor: "rgba(0,0,0,0.5)",
+                    ":hover": {
+                      bgcolor: "rgba(0,0,0,0.7)",
+                    },
+                  }}
+                  component="label"
+                >
+                  <>
+                    <CameraAltIcon />
+                    <VisuallyHiddenInput type="file" />
+                  </>
+                </IconButton>
+              </Stack>
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                value={name.value}
+                onChange={name.changeHandler}
+                margin="normal"
+                variant="outlined"
+              />
+
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+                value={bio.value}
+                onChange={bio.changeHandler}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+                value={username.value}
+                onChange={username.changeHandler}
+              />
+              {username.error && (
+                <Typography color="error" variant="caption">
+                  {username.error}
+                </Typography>
+              )}
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+                value={password.value}
+                onChange={password.changeHandler}
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                fullWidth
+                color="secondary"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Sign Up
+              </Button>
+
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="text"
+                fullWidth
+                onClick={() => setIsLoggedIn(!isLoggedIn)}
+              >
+                Login instead
+              </Button>
+            </form>
+          </>
+        )}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ client/src/utils/validators/validators.js
@@ -0,0 +1,3 @@
+export const usernameValidator = (username) => {
+  return { isValid: false, errorMessage: "Username is Invalid" };
+};
